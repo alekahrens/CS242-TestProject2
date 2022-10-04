9197d52b6326d6a176e818399715f9db2a2abf66
alekahrens <scaaja@gmail.com>
2022-10-04 11:58:04 -0400

    Adding in comments and basic print statement

---
+++ src/HelloMain.java
@@ -0,0 +1,26 @@
+//My first Java code
+import java.util.*;
+/**
+ * The Main class prints "Hello World" on the console
+ * The special tag "@author" describes the author
+ * @author Alek Ahrens
+ * @author Carter Peets
+ */
+
+public class HelloMain {
+    /**
+     * The main Method prints "Hello World" on the console
+     * The special tag @param describes the parameters
+     * @param args command line arguments
+     */
+    public static void main(String[] args){
+        /*
+        Block comments can use multiple lines
+        System: a java class
+        out: an instance of OutputStream
+        println: a method in out
+         */
+        //The following line prints out "Hello World"
+        System.out.println("Hello World");
+    }
+}
